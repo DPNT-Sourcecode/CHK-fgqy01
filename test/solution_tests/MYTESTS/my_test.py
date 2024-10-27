0fb990152c823297a8dddb5d9c7a5f1f8cf8d687
sbx_user1051 <sbx_user1051@169.254.102.141>
2024-10-27 16:00:40 +0000

    Sun Oct 27 16:00:40 UTC 2024

--- test/solution_tests/MYTESTS/my_test.py
+++ test/solution_tests/MYTESTS/my_test.py
@@ -2,8 +2,10 @@ import sys
 import os
 
 # Add the directory two levels up to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
+from lib.solutions.SUM import sum_solution
 
-class TestSum():
-    def test_sum(self):
+class TestAll():
+    def TestAll(self):
         assert sum_solution.compute(1, 2) == 3
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,11 @@
-from solutions.SUM import sum_solution
+import sys
+import os
 
+# Add the directory two levels up to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
+from lib.solutions.SUM import sum_solution
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
