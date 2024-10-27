60031485831e8c57cb058fb631114cc25077ae8e
sbx_user1051 <sbx_user1051@169.254.102.141>
2024-10-27 17:26:40 +0000

    Sun Oct 27 17:26:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -56,14 +56,14 @@ def checkout(skus):
     total_cost = 0
 
     group_count = sum(item_counts.get(item, 0) for item in group_discount_items)
-    total_cost += (group_count // group_discount_qty) * group_discount_price 
-    remaining_group_items = group_count % group_discount_qty
+    total_cost += (group_count // group_discount_qty) * group_discount_price
+    # remaining_group_items = group_count % group_discount_qty
 
     for item in group_discount_items:
         if item in item_counts:
-            used_count = min(item_counts[item], remaining_group_items)
-            item_counts[item] -= used_count
-            remaining_group_items -= used_count
+            used_count = (group_count // group_discount_qty) * group_discount_qty
+            item_counts[item] -= min(item_counts[item], used_count)  # Deduct the items used for the discount
+            # remaining_group_items -= used_count
 
 
     for item, count in item_counts.items():
@@ -78,6 +78,7 @@ def checkout(skus):
             total_cost += count * prices[item]
 
     return total_cost
+
 
 
 
