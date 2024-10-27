d09a0aa7cf010e04f86e2b3eca2628bf6fd98e00
sbx_user1051 <sbx_user1051@169.254.102.141>
2024-10-27 15:45:36 +0000

    Sun Oct 27 15:45:36 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,14 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    offers = {'A': (3, 130), 'B': (2, 45)}
+
+    if not all(item in prices for item in skus):
+        return -1
+
+    total_cost = 0
+    for item, count in item_counts.items():
+
     raise NotImplementedError()
+
