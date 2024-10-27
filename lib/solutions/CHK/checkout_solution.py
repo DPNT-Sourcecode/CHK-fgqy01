0ac1900d1fac63ad2b3fe2056a306f35e0fa9802
sbx_user1051 <sbx_user1051@169.254.102.141>
2024-10-27 16:51:53 +0000

    Sun Oct 27 16:51:53 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,12 +3,36 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10}
-    offers = {
-        'A': [(5, 200), (3, 130)],
-        'B': [(2, 45)],
+    prices = {
+        'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10,
+        'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 80, 'L': 90,
+        'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50,
+        'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90,
+        'Y': 10, 'Z': 50
     }
-    free_offers = {'E': (2, 'B'), 'F': (3, 'F')}
+
+    offers = {
+        'A': [(3, 130), (5, 200)],
+        'B': [(2, 45)],
+        'E': [(2, 'B')],
+        'F': [(2, 'F')],
+        'H': [(5, 45), (10, 80)],
+        'K': [(2, 150)],
+        'N': [(3, 'M')],
+        'P': [(5, 200)],
+        'Q': [(3, 80)],
+        'R': [(3, 'Q')],
+        'U': [(3, 'U')],
+        'V': [(2, 90), (3, 130)]
+    }
+
+    free_offers = {
+        'E': (2, 'B'),
+        'F': (2, 'F'),
+        'N': (3, 'M'),
+        'R': (3, 'Q'),
+        'U': (3, 'U')
+    }
 
     # Validates the input
     if not all(item in prices for item in skus):
@@ -43,3 +67,4 @@ def checkout(skus):
             total_cost += count * prices[item]
 
     return total_cost
+
