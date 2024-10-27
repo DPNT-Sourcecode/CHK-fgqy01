1b123313f3783cd015ca6761c112fa75156518fe
sbx_user1051 <sbx_user1051@169.254.102.141>
2024-10-27 16:09:40 +0000

    Sun Oct 27 16:09:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,18 +19,27 @@ def checkout(skus):
     for item in skus:
         item_counts[item] = item_counts.get(item, 0) + 1
 
-        # Calculates total cost
-        total_cost = 0
-        for item, count in item_counts.items():
-            if item in offers:
-                offer_qty, offer_price = offers[item]
-                offer_times_used = count // offer_qty
-                regular_price_qty = count % offer_qty
-                total_cost += offer_times_used * offer_price + regular_price_qty * prices[item]
-            else:
-                total_cost += count * prices[item]
+    # Applies free offers
+    for item, (required_qty, free_item) in free_offers.items():
+        if item in item_counts:
+            free_items = item_counts[item]
+            if free_item in item_counts:
+                item_counts[free_item] = max(0, item_counts[free_item] - free_items)
 
-        return total_cost
+    # Calculates total cost
+    total_cost = 0
+    for item, count in item_counts.items():
+        if item in offers:
+            offer_qty, offer_price = offers[item]
+            offer_times_used = count // offer_qty
+            regular_price_qty = count % offer_qty
+            total_cost += offer_times_used * offer_price + regular_price_qty * prices[item]
+        else:
+            total_cost += count * prices[item]
+
+    return total_cost
+
+
 
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
     offers = {'A': (3, 130), 'B': (2, 45)}
@@ -57,4 +66,5 @@ def checkout(skus):
 
     return total_cost
     # raise NotImplementedError()
+
 
