1301ce1ca349f95300d42a7db6a589210c0b4cad
sbx_user1051 <sbx_user1051@169.254.102.141>
2024-10-27 16:06:40 +0000

    Sun Oct 27 16:06:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,35 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
+    offers = {
+        'A': [(5, 200), (3, 130)],
+        'B': [(2, 45)],
+    }
+    free_offers = {'E': (2, 'B')}
+
+    # Validates the input
+    if not all(item in prices for item in skus):
+        return -1
+
+    # Counts the items
+    item_counts = {}
+    for item in skus:
+        item_counts[item] = item_counts.get(item, 0) + 1
+
+        # Calculates total cost
+        total_cost = 0
+        for item, count in item_counts.items():
+            if item in offers:
+                offer_qty, offer_price = offers[item]
+                offer_times_used = count // offer_qty
+                regular_price_qty = count % offer_qty
+                total_cost += offer_times_used * offer_price + regular_price_qty * prices[item]
+            else:
+                total_cost += count * prices[item]
+
+        return total_cost
+
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
     offers = {'A': (3, 130), 'B': (2, 45)}
 
@@ -28,3 +57,4 @@ def checkout(skus):
 
     return total_cost
     # raise NotImplementedError()
+
